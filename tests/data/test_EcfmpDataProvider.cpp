a8e21bd5e1f7f8536216ea423b11eda511e33158
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-05-07 21:47:01 +0200

    EcfmpDataProvider: update tests

--- tests/data/test_EcfmpDataProvider.cpp
+++ tests/data/test_EcfmpDataProvider.cpp
@@ -1,30 +1,47 @@
 #include <gtest/gtest.h>
 
+#include <memory>
 #include <nlohmann/json.hpp>
 
+#include "api/IRestApi.h"
 #include "data/EcfmpDataProvider.h"
+#include "tests/api/EcfmpExampleJsons.h"
+#include "tests/api/RestApiMock.h"
 #include "types/FlowMeasure.h"
 
 using json = nlohmann::json;
 using namespace ecfmp;
 using namespace ecfmp::data;
 
-TEST(EcfmpDataProviderTest, getMeasures) {
-    EcfmpDataProvider provider;
+class EcfmpDataProviderTest : public ::testing::Test {
+   protected:
+    tests::api::RestApiMock* m_apiRaw;
+    std::unique_ptr<EcfmpDataProvider> m_ecfmpDataProvider;
 
-    const std::vector<types::FlowMeasure> result = provider.getMeasures();
+    void SetUp() override {
+        auto mock = std::make_unique<tests::api::RestApiMock>();
+        m_apiRaw = mock.get();
+        m_ecfmpDataProvider = std::make_unique<EcfmpDataProvider>(std::move(mock));
+    }
+};
 
-    // EXPECT_FALSE(result.empty());
-    // EXPECT_EQ(result.size(), 4);
+TEST_F(EcfmpDataProviderTest, getMeasures) {
+    m_apiRaw->setReturnJson(tests::api::example_data::flowMeasureArray);
+
+    const std::vector<types::FlowMeasure> result = m_ecfmpDataProvider->getMeasures();
+
+    EXPECT_EQ(result.size(), 5);
+    EXPECT_EQ(result[0].id, 1672);
+    EXPECT_EQ(result[1].id, 1673);
+    EXPECT_EQ(result[2].id, 1675);
+    EXPECT_EQ(result[3].id, 1676);
+    EXPECT_EQ(result[4].id, 1677);
 }
 
-TEST(EcfmpDataProviderTest, PerformGetReturnsValidJson) {
-    EcfmpDataProvider provider;
+TEST_F(EcfmpDataProviderTest, getMeasure) {
+    const int64_t expectedMeasureId = 1675;
+    m_apiRaw->setReturnJson(tests::api::example_data::singleFlowMeasure);
+    const types::FlowMeasure result = m_ecfmpDataProvider->getMeasure(expectedMeasureId);
 
-    types::FlowMeasure expected = types::FlowMeasure();
-    expected.id = 1671;
-
-    const types::FlowMeasure result = provider.getMeasure(1671);
-
-    // EXPECT_EQ(result.id, expected.id);
+    EXPECT_EQ(result.id, expectedMeasureId);
 }
